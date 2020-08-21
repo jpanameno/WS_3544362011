7e2f012916485abde92af2d2ff23bfd9b67d74e8
jpanameno <vpl.jose@gmail.com>
2020-08-21 13:06:16 -0400

    Agregar cambios para funcionalidad de combobox en cliente

--- WS_3544362011/Servicios.asmx.cs
+++ WS_3544362011/Servicios.asmx.cs
@@ -30,8 +30,21 @@ namespace WS_3544362011
         {
             SqlConnection conn = new SqlConnection("Data Source=MRISQL;Initial Catalog=Adventureworks2014;Integrated Security=true;");
             conn.Open();
-            SqlCommand sql = new SqlCommand("Select SUM(UnitPrice * OrderQty) AS totalFactura FROM Sales.SalesOrderHeader INNER JOIN Sales.SalesOrderDetail ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID where SalesOrderHeader.SalesOrderID=@order GROUP BY SalesOrderHeader.SalesOrderID", conn);
+            SqlCommand sql = new SqlCommand("Select Freight, TotalDue, TaxAmt, SUM(UnitPrice * OrderQty) AS totalFactura FROM Sales.SalesOrderHeader INNER JOIN Sales.SalesOrderDetail ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID where SalesOrderHeader.SalesOrderID=@order GROUP BY SalesOrderHeader.SalesOrderID, Freight, TotalDue, TaxAmt", conn);
             sql.Parameters.AddWithValue("@order", Orden);
+            SqlDataAdapter da = new SqlDataAdapter(sql);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            return ds;
+        }
+
+        [WebMethod]
+        public DataSet GetNumeroOrdenes()
+        {
+            SqlConnection conn = new SqlConnection("Data Source=MRISQL;Initial Catalog=Adventureworks2014;Integrated Security=true;");
+            conn.Open();
+            SqlCommand sql = new SqlCommand("Select top(100) SalesOrderID FROM Sales.SalesOrderHeader GROUP BY SalesOrderID", conn);
             SqlDataAdapter da = new SqlDataAdapter(sql);
             DataSet ds = new DataSet();
             da.Fill(ds);
